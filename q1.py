6758000d8894d9969aff0d91f1f6c61f277ed95d
egaygun24 <egaygun24@ku.edu.tr>
2026-04-02 18:17:51 +0300

    prelab 5

--- q1.py
+++ q1.py
@@ -2,4 +2,14 @@ def longest_palindromic_substring(s):
     """
     Given a string find the longest palindromic substring
     """
-    pass
+    res = ""
+    
+    for i in range(len(s)):
+        for l, r in ((i, i), (i, i+1)):
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if r - l + 1 >= 2 and r - l + 1 > len(res):
+                    res = s[l:r+1]
+                l -= 1
+                r += 1
+                
+    return res
--- q3b.py
+++ q3b.py
@@ -2,4 +2,13 @@ def add_binary(a, b):
     '''
     Given two strings perform binary addition and return the result as a string
     '''
-    pass
+    a, b = a[2:], b[2:]
+    res = 0
+    for i in range(len(a)):
+        res += int(a[len(a)-1-i]) * (2 ** i)
+    res2 = 0
+    for j in range(len(b)):
+        res2 += int(b[len(b)-1-j]) * (2 ** j)
+    c = res + res2
+    return "0b" + format(c, 'b')
+        
